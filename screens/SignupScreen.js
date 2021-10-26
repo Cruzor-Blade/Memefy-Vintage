6885e4269cc31c00b5b9e39db9cdc13cfe7dd573
SONKENG MBOGNING Brice <cruzorbladex@gmail.com>
2021-10-26 16:30:30 +0100

    Fully working post and get images from the database

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -11,7 +11,7 @@ const SignupScreen = ({navigation}) => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const {register} = useContext(AuthContext)
+  const {register} = useContext(AuthContext);
 
     return(
         <View style={styles.container}>
